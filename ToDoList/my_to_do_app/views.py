27f06adbce488dc02c5715c3a5554ed27d3a3bae
newpullin <newpullin@naver.com>
2020-08-18 11:55:18 +0900

    update function update

--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -17,9 +17,28 @@ def createTodo(request):
     return HttpResponseRedirect(reverse('index'))
     #return HttpResponse(user_input_str)
 
-def deleteTodo(request):
-    done_todo_id = request.GET['todoNum']
-    print("완료한 todo의 id: ", done_todo_id)
-    todo = Todo.objects.get(id = done_todo_id)
-    todo.delete()
+def doneTodo(request):
+
+    if '완료' == request.GET['btn_type']:
+        done_todo_id = request.GET['todoNum']
+        print("완료한 todo의 id: ", done_todo_id)
+        todo = Todo.objects.get(id = done_todo_id)
+        todo.isDone = not todo.isDone
+        todo.save()
+    elif '수정' == request.GET['btn_type']:
+        update_todo_id = request.GET['todoNum']
+        todo = Todo.objects.get(id=update_todo_id)
+        if todo.isUpdate :
+            todo.content = request.GET['editContent']
+            todo.isUpdate = False
+            todo.save()
+        else:
+            for obj in Todo.objects.all():
+                obj.isUpdate = False
+                obj.save()
+            todo.isUpdate = True
+            todo.save()
+
+
+
     return HttpResponseRedirect(reverse('index'))
